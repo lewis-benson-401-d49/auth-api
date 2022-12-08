7fd4a14a706d52da6159b8140f9a1e34b0c9323b
Lewis Benson <tm.lewis.benson@gmail.com>
2022-12-07 20:02:36 -0500

    add food and clothes schema to model

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -2,12 +2,20 @@
 
 const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
+const foodModel = require('../models/food/model');
+const clothesModel = require('../models/clothes/model');
+const Collection = require('./data-collection.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
 
 const sequelize = new Sequelize(DATABASE_URL);
 
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
+  food: new Collection(food),
+  clothes: new Collection(clothes),
 };
